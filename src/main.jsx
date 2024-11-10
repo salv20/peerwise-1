552b78b643098bd3f7420655accb4e3cec3d93f4
Salvation <salvationamoke@gmail.com>
2024-11-10 17:12:57 +0100

    folder structure arrangement

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route index Component={App} />
+      <Route path="signup" Component={SignUpPage} />
+      <Route path="dashboard" Component={DashboardPage} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/LogInPage.jsx
@@ -0,0 +1,9 @@
+const LogInPage = () => {
+  return (
+    <div>
+      <h1>LogInPage</h1>
+    </div>
+  );
+};
+
+export default LogInPage;
